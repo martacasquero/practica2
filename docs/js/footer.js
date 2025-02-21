d107fd55b946b475b230a55a96014fe9455d6528
martacasquero <martacasqueroo@gmail.com>
2025-02-21 19:19:27 +0100

    cuarto commit

---
+++ docs/js/footer.js
@@ -0,0 +1,8 @@
+ document.addEventListener("DOMContentLoaded", function() {
+     fetch("../footer.html")  //obtengo el contenido de lo que contiene footer.html 
+        .then(response => response.text())   // con esta linea convierto el contenido en texto plano 
+        .then(data => {
+             document.body.insertAdjacentHTML("beforeend", data);// inserto el contenido antes d que se termine el html 
+        })
+        .catch(error => console.error("Error al cargar el footer:", error));  
+});
