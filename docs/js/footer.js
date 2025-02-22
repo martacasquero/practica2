309de3d093dbac5c54bd1c4e4edd0baa69c74bb4
martacasquero <martacasqueroo@gmail.com>
2025-02-22 16:56:48 +0100

    sexto

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -1,5 +1,5 @@
  document.addEventListener("DOMContentLoaded", function() {
-     fetch("../footer.html")  //obtengo el contenido de lo que contiene footer.html 
+     fetch("/footer.html")  //obtengo el contenido de lo que contiene footer.html 
         .then(response => response.text())   // con esta linea convierto el contenido en texto plano 
         .then(data => {
              document.body.insertAdjacentHTML("beforeend", data);// inserto el contenido antes d que se termine el html 
