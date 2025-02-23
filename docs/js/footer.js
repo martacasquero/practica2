499d8dee9a389fc55b159324ddcd9e51c3f27ba7
martacasquero <martacasqueroo@gmail.com>
2025-02-23 18:12:05 +0100

    dec

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("footer").innerHTML = `
-      <footer style="background-color: #333; color: #fff; text-align: center; padding: 20px 0;">
+      <footer>
         <div style="max-width: 1200px; margin: 0 auto;">
           <p>&copy; 2025 Mi Sitio Web. Todos los derechos reservados.</p>
           <nav>
-            <a href="index.html" style="color: #fff; margin: 0 10px; text-decoration: none;">Inicio</a>
-            <a href="sobreNosotros.html" style="color: #fff; margin: 0 10px; text-decoration: none;">Sobre Nosotros</a>
-            <a href="tallas.html" style="color: #fff; margin: 0 10px; text-decoration: none;">Tallas</a>
-            <a href="registro.html" style="color: #fff; margin: 0 10px; text-decoration: none;">Registro</a>
+            <a href="index.html">Inicio</a>
+            <a href="sobreNosotros.html">Sobre Nosotros</a>
+            <a href="tallas.html">Tallas</a>
+            <a href="registro.html">Registro</a>
           </nav>
         </div>
       </footer>
